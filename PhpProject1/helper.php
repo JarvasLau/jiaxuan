d1acbeb3be5d486e34f6b48db98fc27e1e6154f3
JarvasLau <jarvaslau515@gmail.com>
2025-05-01 18:37:29 +0800

    Update helper.php

--- PhpProject1/helper.php
+++ PhpProject1/helper.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'dbcloud.cle2sgso8sju.us-east-1.rds.amazonaws.com');
 define('DB_USER', 'jarvas');
 define('DB_PASS', 'a3014402');
-define('DB_NAME', 'labs');
+define('DB_NAME', 'cloud');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
